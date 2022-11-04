e3e6b6bc6d9f5840b53214cc99174e11013d9b22
Your Name <mohamedsellouk99@gmail.com>
2022-11-04 11:32:52 +0100

    Initial commit

---
+++ src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,54 @@
+package org.sid.inventoryservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository) {
+		return args -> {
+			productRepository.save(new Product(null, "Ordinateur", 982, 12));
+			productRepository.save(new Product(null, "Tablet", 996, 24));
+			productRepository.save(new Product(null, "Imprimante", 902, 5));
+			productRepository.findAll().forEach(p -> {
+				System.out.println(p.getName());
+			});
+		};
+	}
+}
+
+	@Entity
+	@Data @NoArgsConstructor @AllArgsConstructor @ToString
+	class Product{
+		@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		private String name;
+		private double price;
+		private double quantity;
+	}
+    @RepositoryRestResource
+	interface ProductRepository extends JpaRepository<Product,Long>{
+
+	}
+
+
